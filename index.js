ebab19aa16f207e51476051f4cadd29d94938d89
edurazo12 <74686150+edurazo12@users.noreply.github.com>
2021-04-15 23:31:43 -0700

    MVP is finished

--- index.js
+++ index.js
@@ -42,8 +42,25 @@ class Airplane {
   */
   
  class Person {
-    
-  }
+   constructor(name, age) {
+     this.name = name;
+     this.age = age;
+     this.stomach = [];
+
+   }
+   eat(edible) {
+     if(this.stomach.length < 10) {
+       this.stomach.push(edible);
+     }
+   }
+
+   poop() {
+     return this.stomach = [];
+   }
+   toString() {
+     return `${this.name}, ${this.age}`;
+   }
+}
   
   /*
     TASK 2
@@ -60,8 +77,27 @@ class Airplane {
   */
   
  class Car {
-    
-  }
+   constructor(model, milesPerGallon) {
+     this.model = model;
+     this.milesPerGallon = milesPerGallon;
+     this.tank = 0;
+     this.odometer = 0;
+   }
+   fill(gallons) {
+     this.tank += gallons;
+     return this;
+   }
+   drive(distance) {
+     if(distance < (this.tank * this.milesPerGallon)) {
+       this.odometer += distance;
+       this.tank -= distance / this.milesPerGallon;
+     } else {
+       this.odometer += this.tank * this.milesPerGallon;
+       this.tank = 0;
+       return `I ran out of fuel at ${this.odometer} miles!`;
+     }
+   }    
+}
   
   /*
     TASK 3
@@ -76,8 +112,14 @@ class Airplane {
           + {name} and {location} of course come from the instance's own properties.
   */
  class Lambdasian {
-    
-  }
+   constructor(properties) {
+     this.name = properties.name;
+     this.age = properties.age;
+     this.location = properties.location;
+   }
+   speak() {
+     return `Hello my name is ${this.name}, I am from ${this.location}`;
+   }
   
   /*
     TASK 4
@@ -93,8 +135,20 @@ class Airplane {
           + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
           + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
   */
- class Instructor {
 
+class Instructor extends Lambdasian {
+   constructor(properties) {
+     super(properties);
+       this.specialty = properties.specialty;
+       this.favLanguage = properties.favLanguage;
+       this.catchPhrase = properties.catchPhrase;
+   }
+   demo(subject) {
+     return `Today we are learning about ${subject}`;
+   }
+   grade(student, subject) {
+     return `${student.name} revieces a perfect score on ${subject}`;
+   }
  }
   /*
     TASK 5
@@ -111,7 +165,23 @@ class Airplane {
           + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
           + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
   */
- class Student {
+ class Student extends Lambdasian {
+   constructor(properties) {
+     super(properties);
+     this.previourBackground = properties.previourBackground;
+     this.className = properties.className;
+     this.favSubjects = properties.favSubjects;
+   }
+   listSubjects() {
+     return `Loving ${this.favSubjects}!`;
+   }
+   PRAssignment(subject) {
+     return `${this.name} has submitted a PR for ${subject}`;
+   }
+   sprintChallenge(subject) {
+     return `${this.name} has begun sprint challenge on ${subject}`;
+     
+   }
      
  }
   
@@ -128,8 +198,15 @@ class Airplane {
           + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
           + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
   */
- class ProjectManager {
-     
+ class ProjectManager extends Instructor {
+   constructor(properties) {
+     super(properties);
+     this.gradClassName = properties.gradClassName;
+     this.favInstructor = properties.favInstructor;
+   }
+   standUp(channel) {
+     return `${this.name} debugs ${student.name}'s code on ${subject}`;
+   }
  }
   /*
     STRETCH PROBLEM (no tests!)
